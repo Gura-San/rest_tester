3832002235cb6bab3225f4d5514a37849fb44ef2
David Guras <gura.san@gmail.com>
2019-04-03 09:52:56 -0400

    rest-tester: created 1 controller ÇREATE action and view, updated the requestor script to accommodate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,23 @@
 class UsersController < ApplicationController
 
   def index
-    @name = "I am the Index action!"
+	@name = "I am the Index action!"
   end
 
   def new
-    @name = 'I am the New Action'
+	@name = 'I am the New Action'
   end
 
   def edit
-    @name = 'I am the Edit Action'
+	@name = 'I am the Edit Action'
   end
 
   def show
-    @name = 'I am the Show Action'
+	@name = 'I am the Show Action'
+  end
+
+  def create
+	@name = "I am the Create Action"
   end
 
 end
--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -4,5 +4,6 @@ url2 = "http://localhost:3000/users/new"
 url3 = "http://localhost:3000/:id/edit"
 url4 = "http://localhost:3000/users/:id"
 
-puts RestClient.get(url2)
+# puts RestClient.get(url2)
 
+puts RestClient.post(url1, {})
